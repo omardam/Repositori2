d5440e2c65841c7050ae9fa89a9465c0e8c5c388
unknown <xyzomarr@gmail.com>
2024-03-06 19:25:15 +0100

    Test

---
+++ Conversio.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+package com.mycompany.conversio;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author nil
+ */
+public class Conversio {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Introdueix en £");
+        int valor_lliuras = sc.nextInt();
+
+        double tipus_de_canvi = 1.17d;
+        System.out.println(valor_lliuras + " lliuras (£) acatualmentt ewuivalen a" + (valor_lliuras * tipus_de_canvi) + " euros (€)");
+    }
+
+}
